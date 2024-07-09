21299a32169c97a322b57f7d0823aed500f51f0e
Young Joon Lee <entelecheia@hotmail.com>
2024-07-09 11:44:14 +0900

    refactor(language_switcher): improve path handling for language switcher

--- book/en/_static/language_switcher.js
+++ book/en/_static/language_switcher.js
@@ -1,12 +1,12 @@
 // book/_addons/language_switcher.js
 function switchLanguage(newLang) {
-    var currentPath = window.location.origin + window.location.pathname;
+    var currentPath = window.location.pathname;
     var supportedLangs = ['en', 'ko']; // Update this array to match your LANGUAGES array
     var langRegex = new RegExp('\\b(' + supportedLangs.join('|') + ')\\b');
 
     if (langRegex.test(currentPath)) {
         // If the current path contains a language code, replace it
-        var newPath = currentPath.replace(langRegex, newLang);
+        var newPath = window.location.origin + currentPath.replace(langRegex, newLang);
         window.location.href = newPath + window.location.search + window.location.hash;
     }
 }
